a7fd59ad725e6065147b5d39c2c6477e7e985114
Boniface Kimani Muguro <mmbonifacekimani@gmail.com>
2025-03-05 17:46:59 +0300

    days of the week

---
+++ Development/code/phase-1/javascript/control-flow.js
@@ -0,0 +1,19 @@
+const day = 2;
+
+if day === 1 {
+  console.log('It is Sunday');
+} else if day === 2 {
+  console.log('It is Monday');
+} else if day === 3 {
+  console.log('It is Tuesday');
+} else if day === 4 {
+  console.log('It is Wednesday');
+} else if day === 5 {
+  console.log('It is Thursday');
+} else if day === 6 {
+  console.log('It is Friday');
+} else if day === 7 {
+  console.log('It is Saturday');
+} else {
+  console.log('Out of bounds(1-7)');
+}
